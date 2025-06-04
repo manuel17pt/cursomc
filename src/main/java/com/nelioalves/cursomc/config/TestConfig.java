ddce2e2b3c98fd6ce401f084da04d8b08aa5f079
manuel17pt <manuelantoniodosss@gmail.com>
2025-06-04 23:36:38 +0100

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.nelioalves.cursomc.config;
 import com.nelioalves.cursomc.services.DBService;
 import com.nelioalves.cursomc.services.EmailService;
 import com.nelioalves.cursomc.services.MockEmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,7 @@ public class TestConfig {
 
     @Bean
     public EmailService emailService() {
-        return new MockEmailService();
+        //return new MockEmailService();
+        return new SmtpEmailService();
     }
 }
